17fb1a3d043b76a0a8d229fd4cc8bf022419a551
Maggy Tjin <tjin.maggy@gmail.com>
2024-03-25 13:06:23 -0700

    can create a new grid size

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const container = document.querySelector(".container");
 
-function createColumn(num) {
+function createGrid(num) {
     for (let i = 0; i < num; i++) {
-        let div = document.createElement("div");
-        div.classList.add("column");
-        div.style.width = 500 / num + "px";
-        div.style.height = 500 / num + "px";
-        container.appendChild(div);
+        let column = document.createElement("div");
+        column.classList.add("column");
+        column.style.width = 500 / num + "px";
+        column.style.height = 500 / num + "px";
+        container.appendChild(column);
 
         function createRow(num) {
             for (let r = 0; r < num -1; r++) {
@@ -19,22 +19,60 @@ function createColumn(num) {
         }
         createRow(num);
     }
-};
 
-createColumn(30);
-
-
-
-const boxes = document.querySelectorAll(".container > div");
-
-
-boxes.forEach(box => {
+    const boxes = document.querySelectorAll(".container > div");
+    boxes.forEach(box => {
     box.addEventListener("mouseenter", () => {
         box.style.backgroundColor = "green";
         setTimeout(() => {
             box.style.backgroundColor = "";
         }, 500)
+        })
     })
-});
+};
 
-// setTimeout()
+createGrid(16);
+
+
+const num = document.querySelector(".dimensions");
+const newGrid = document.querySelector(".newGrid");
+
+num.addEventListener("click", () => {
+    let num = prompt("New grid size", "Enter a number");
+    container.remove();
+
+    function createGrid(num) {
+        const container = document.createElement("div");
+        container.classList.add("container");
+        newGrid.appendChild(container);
+        for (let i = 0; i < num; i++) {
+            let column = document.createElement("div");
+            column.classList.add("column");
+            column.style.width = 500 / num + "px";
+            column.style.height = 500 / num + "px";
+            container.appendChild(column);
+    
+            function createRow(num) {
+                for (let r = 0; r < num -1; r++) {
+                    let row = document.createElement("div");
+                    row.classList.add("row");
+                    row.style.width = 500 / num + "px";
+                    row.style.height = 500 / num + "px";
+                    container.appendChild(row);
+                }
+            }
+            createRow(num);
+        }
+        const boxes = document.querySelectorAll(".container > div");
+        boxes.forEach(box => {
+            box.addEventListener("mouseenter", () => {
+                box.style.backgroundColor = "green";
+                setTimeout(() => {
+                    box.style.backgroundColor = "";
+                }, 500)
+            })
+        });
+    };
+    
+    createGrid(num);
+})
